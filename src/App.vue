738a80ca54c25e12d1e2aa94c9b2a16502b3ccc5
moeinroid <moeinroid@gmail.com>
2019-01-08 15:58:56 +0330

    Use Vazir as main font

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,6 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: 'Vazir', sans-serif;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 
 import 'bulma-rtl/css/bulma-rtl.css'
+import './assets/font-style.css'
 
 Vue.config.productionTip = false
 
