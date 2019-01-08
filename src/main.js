a45d213eef64c42138ace4b0eb5663e2b9cd0c9f
moeinroid <moeinroid@gmail.com>
2019-01-08 16:15:59 +0330

    Sync connection in main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { connection } from './database'
+
 import 'bulma-rtl/css/bulma-rtl.css'
 import './assets/font-style.css'
+
+connection.sync()
 
 Vue.config.productionTip = false
 
