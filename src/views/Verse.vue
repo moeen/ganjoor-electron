38b5c8f5d33197271c3247c0f1818c0af1aab8be
moeinroid <moeinroid@gmail.com>
2019-01-08 16:13:15 +0330

    Add verse view

---
+++ src/views/Verse.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="verse in verses" :key="verse.id">
+      <div style="width: 50%; float: right; text-align: left; padding-left: 5%; margin-bottom: 10px;" v-if="verse.position == 0">
+        {{verse.text}}
+      </div>
+      <div style="width: 50%; float: left; text-align: right; padding-right: 5%; margin-bottom: 10px;" v-if="verse.position == 1">
+        {{verse.text}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapState, mapActions } from 'vuex'
+  export default {
+    name: 'verse',
+    data () {
+      return {
+        id: null
+      }
+    },
+    beforeMount () {
+      this.id = this.$route.params.id
+      this.getVerses(this.id)
+    },
+    computed: mapState({ verses: state => state.verseModule.verses }),
+    methods: mapActions(['getVerses'])
+  }
+</script>
