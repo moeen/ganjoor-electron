52d8133934457ad5971254b07ea3066c968d6028
moeinroid <moeinroid@gmail.com>
2019-02-11 19:05:23 +0330

    Get DB path from userData

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,16 @@
 import Sequelize from 'sequelize'
 import path from 'path'
+import electron from 'electron'
 
 import poetModel from './models/Poet'
 import catModel from './models/Cat'
 import poemModel from './models/Poem'
 import verseModel from './models/Verse'
 
-const databaseAddress = path.join(__static, 'ganjoor.s3db')
+const isRenderer = process && process.type === 'renderer'
+const userData =  (isRenderer ? electron.remote : electron).app.getPath('userData')
+
+const databaseAddress = path.join(userData, 'ganjoor.s3db')
 
 export const connection = new Sequelize('ganjoor', null, null, {
   dialect: 'sqlite',
